7f4ea2f8eaea38872512e0ecf58dc2340e04f87f
iltya <iltyakov.nik@mail.ru>
2022-07-18 14:47:01 +0700

    plot maker refactor

--- plot_maker.py
+++ plot_maker.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 
 
 class PlotMaker:
-    '''
+    """
     A class for drawing graphs of loss functions and model metrics
-    >>> plot_maker = PlotMaker("Test title", ["first optimizer", "second_optimizer", "third optimizer"], metric_name="IoU", loss_name="focal_loss")
-    >>> plot_maker.draw_plot(history, history, history)
-    '''
+    >>> plot_maker = PlotMaker("Test title", ["first optimizer name", "second_optimizer name", "third optimizer namae"], \
+                     metric_name="IoU", loss_name="focal_loss")
+    >>> history_example = [[1, 2, 3],       # for first optimizers
+    >>>                    [1, 2, 3],       # for second optimizer
+    >>>                    [1, 2, 3]]       # for third optimizer
+    >>> plot_maker.draw_plot(history_one, history_two, history_three)
+    """
 
+    font_dict = {'fontsize': 14, 'fontweight': 'medium'}
     def __init__(self, main_title, names_optimizers, metric_name="accuracy", loss_name=""):
         self.main_title = main_title
         self.names_optimizers = names_optimizers
@@ -22,22 +27,15 @@ class PlotMaker:
         ax2 = plt.subplot2grid((2, 5), (0, 3), colspan=2)
         ax3 = plt.subplot2grid((2, 5), (1, 3), colspan=2)
 
-        font_dict = {'fontsize': 14, 'fontweight': 'medium'}
-        ax1.set_title(f"Train loss {self.loss_name}", fontdict=font_dict)
-        ax2.set_title(f"Train {self.metric_name}", fontdict=font_dict)
-        ax3.set_title(f"Valid {self.metric_name}", fontdict=font_dict)
+        ax1.set_title(f"Train loss {self.loss_name}", fontdict=self.font_dict)
+        ax2.set_title(f"Train {self.metric_name}", fontdict=self.font_dict)
+        ax3.set_title(f"Valid {self.metric_name}", fontdict=self.font_dict)
 
-        for i in range(len(train_losses)):
-            ax1.plot(train_losses[i], label=self.names_optimizers[i], alpha=0.5)
-        ax1.legend()
-        ax1.grid()
+        axes = [ax1, ax2, ax3]
+        all_histories = [train_losses, train_acc, val_acc]
 
-        for i in range(len(train_acc)):
-            ax2.plot(train_acc[i], label=self.names_optimizers[i], alpha=0.5)
-        ax2.legend()
-        ax2.grid()
-
-        for i in range(len(val_acc)):
-            ax3.plot(val_acc[i], label=self.names_optimizers[i], alpha=0.5)
-        ax3.legend()
-        ax3.grid()
+        for ax, loss_histories in zip(axes, all_histories):
+            for ind, loss_history in enumerate(loss_histories):
+                ax.plot(loss_history, label=self.names_optimizers[ind], alpha=0.5)
+                ax.legend()
+                ax.grid()
